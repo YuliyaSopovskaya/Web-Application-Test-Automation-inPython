61c5f59f86c57757bb70b12876fce1a0fce72280
YuliyaSopovskaya <109427956+YuliyaSopovskaya@users.noreply.github.com>
2023-11-19 17:51:28 +0300

    Update main.py

--- selenium/main.py
+++ selenium/main.py
@@ -1,7 +1,10 @@
 import yaml
 from module import Site
 
-with open ('./testdata.yaml') as f:
+with open('testdata.yaml') as f:
     testdata = yaml.safe_load(f)
 
 site = Site(testdata["address"])
+
+css_selector = "span.mdc-text-field__ripple"
+print(site.get_element_property("css", css_selector, "height"))
