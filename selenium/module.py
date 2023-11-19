9a10832397c34fa4782b9db70362ec127fc7a493
YuliyaSopovskaya <109427956+YuliyaSopovskaya@users.noreply.github.com>
2023-11-19 17:52:24 +0300

    Update module.py

--- selenium/module.py
+++ selenium/module.py
@@ -1,16 +1,41 @@
 import yaml
 import time
-from select import webdriver
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 
-with open ('./testdata.yaml') as f:
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+with open('./testdata.yaml') as f:
     testdata = yaml.safe_load(f)
 
-service = Service(testdata["driver_path"])
+# service = Service(testdata["driver_path"])
+service = ChromeService(ChromeDriverManager().install())
 options = webdriver.ChromeOptions()
+
 class Site:
     def __init__(self, address):
-        self.driver = webdriver.Chrome(servise=Service, options=options)
-        self.driver.maximixe_window()
+        self.driver = webdriver.Chrome(service=service, options=options)
+
+        # self.driver = webdriver.Chrome(service=Service(testdata["driver_path"]), options=options)
+        self.driver.maximize_window()
         self.driver.get(address)
         time.sleep(testdata["sleep_time"])
+
+    def find_element(self, mode, path):
+        if mode == "css":
+            element = self.driver.find_element(By.CSS_SELECTOR, path)
+        elif mode == "xpath":
+            element = self.driver.find_element(By.XPATH, path)
+        else:
+            element = None
+        return element
+
+    def get_element_property(self, mode, path, property):  #режим поиска / путь / свойства
+        element = self.find_element(mode, path)
+        return element.value_of_css_property(property)
+
+    def close(self):
+        self.driver.close()
